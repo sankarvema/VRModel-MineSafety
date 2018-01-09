5d3670d4bd2e4f31955890abed3e74571226d6b4
Sankar Vema <svema@csc.com>
2018-01-09 17:58:53 +0530

    Drone survey added

---
+++ MineDrone/Assets/DroneMovementScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovementScript : MonoBehaviour {
+
+    Rigidbody ourDrone;
+
+    private void Awake()
+    {
+        ourDrone = GetComponent<Rigidbody>();
+        droneSound = gameObject.transform.FindChild("droneSound").GetComponent<AudioSource>();
+    }
+
+
+    //ToDo: Tilting the drone is not working as expected
+    private void FixedUpdate()
+    {
+        MovementUpDown();
+        MovementForward();
+        Rotation();
+        ClampingSpeedValues();
+
+        ourDrone.AddRelativeForce(Vector3.up * upForce);
+        ourDrone.rotation = Quaternion.Euler(
+                new Vector3(tiltAmountForward, currentYRotation, ourDrone.rotation.z)
+            );
+    }
+
+    public float upForce;
+    void MovementUpDown()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            upForce = 250;
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            upForce = -110;
+        }
+        else if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K))
+        {
+            upForce = 98.1f ;
+        }
+    }
+
+    private float movementForwardSpeed = 500.0f;
+    private float tiltAmountForward = 0;
+    private float tiltVelocityForward;
+    void MovementForward()
+    {
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            ourDrone.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * movementForwardSpeed);
+            //tiltAmountForward = Mathf.SmoothDamp(tiltAmountForward, 20 * Input.GetAxis("Vertical"), ref tiltVelocityForward, 0.1f);
+        }
+    }
+
+    private float wantedYRotation;
+    private float currentYRotation;
+    private float rotationAmountByKeys = 0.5f;
+    private float rotationByVelocity;
+    void Rotation()
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            wantedYRotation -= rotationAmountByKeys;
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            wantedYRotation += rotationAmountByKeys;
+        }
+
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, wantedYRotation, ref rotationByVelocity, 0.05f);
+
+    }
+
+    private Vector3 velocityToSmoothDampToZero;
+    void ClampingSpeedValues()
+    {
+        if(Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10.0f, Time.deltaTime * 5f));
+        }
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)
+        {
+            ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 10.0f, Time.deltaTime * 5f));
+        }
+        if (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            ourDrone.velocity = Vector3.ClampMagnitude(ourDrone.velocity, Mathf.Lerp(ourDrone.velocity.magnitude, 5.0f, Time.deltaTime * 5f));
+        }
+        if (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)
+        {
+            ourDrone.velocity = Vector3.SmoothDamp(ourDrone.velocity, Vector3.zero, ref velocityToSmoothDampToZero, 0.95f);
+        }
+    }
+
+    private AudioSource droneSound;
+    void DroneSound()
+    {
+        droneSound.pitch = 1 + (ourDrone.velocity.magnitude / 100);
+    }
+}
